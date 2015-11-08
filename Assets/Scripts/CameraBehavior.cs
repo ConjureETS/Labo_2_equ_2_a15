9b2cd431a56132748bdc6f17b455715d704d000f
Marc-Antoine Dumont <dumont.marcantoine13@gmail.com>
2015-11-08 14:46:07 -0500

    Initial Commit

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+
+    public GameObject player;
+    private float zAxis;
+
+	// Use this for initialization
+	void Start () {
+        zAxis = gameObject.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        gameObject.transform.position = new Vector3(player.transform.position.x, 0, zAxis);
+	}
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    public float maxSpeed = 50.0f;
+
+    // Jump Stuff
+    public bool grounded = true;
+    public Transform groundCheck;
+    private float groundRadius = 0.1f;
+    public LayerMask ground;
+    public float jumpForce = 7.0f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(grounded && Input.GetAxis("Vertical") > 0.0f)
+        {
+            anim.SetBool("Ground", false);
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, ground);
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("vSpeed", rb.velocity.y);
+
+        float xMove = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xMove * maxSpeed, rb.velocity.y);
+
+        if ((xMove < 0 && facingRight) || (xMove > 0 && !facingRight))
+            flip();
+
+        // Change animation according to user inputs
+        anim.SetFloat("Speed", Mathf.Abs(xMove));
+    }
+
+    // ----- Private function(s)
+    private void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
