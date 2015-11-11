c03191fa8bbfb0377f20ef0c2b33a57daa93e736
MartinRemi <mrmi11@yahoo.fr>
2015-11-10 20:01:16 -0500

    Added ennemy movement

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class EnemyBehavior : MonoBehaviour {
 
+    private float direction = 1.0f;
+    private Rigidbody2D rb;
+
+    // Ground
+    public GameObject ground;
+    private Bounds groundBounds;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        rb = GetComponent<Rigidbody2D>();
+
+        groundBounds = ground.GetComponent<Renderer>().bounds;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        if ((direction > 0 && transform.position.x >= groundBounds.max.x) ||
+            (direction < 0 && transform.position.x <= groundBounds.min.x))
+            direction *= -1.0f;
+
+        rb.velocity = Vector2.right * direction;
 	}
 }
