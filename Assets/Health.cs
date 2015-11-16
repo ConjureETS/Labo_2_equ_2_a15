ab20f6aca933481a7eee554b92f9fd5410979fe4
MartinRemi <mrmi11@yahoo.fr>
2015-11-16 15:20:07 -0500

    Added health script

---
+++ Assets/Health.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+    public int maxHP;
+    private int hp;
+
+	// Use this for initialization
+	void Start () {
+        hp = maxHP;
+	}
+	
+	public int removeHP(int amount)
+    {
+        hp -= amount;
+        if (hp < 0)
+            hp = 0;
+        return hp;
+    }
+
+    public int addHP(int amount)
+    {
+        hp += amount;
+        if (hp > maxHP)
+            hp = maxHP;
+        return hp;
+    }
+
+    public int getHP()
+    {
+        return hp;
+    }
+}
