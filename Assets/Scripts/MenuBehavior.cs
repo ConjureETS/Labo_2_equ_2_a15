91f044026ecd3fe085900d35ab56448b40878281
alsarazin <alexandre.sarazin1@gmail.com>
2015-11-17 12:02:02 -0500

    Added a start menu

---
+++ Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBehavior : MonoBehaviour {
+
+	public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
