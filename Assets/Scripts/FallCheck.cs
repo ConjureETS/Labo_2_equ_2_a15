687dbe82b779d68ffbccb859cc2bc92251e9bd16
MartinRemi <mrmi11@yahoo.fr>
2015-11-30 18:24:35 -0500

    Removed life when falling

--- Assets/Scripts/FallCheck.cs
+++ Assets/Scripts/FallCheck.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FallCheck : MonoBehaviour {
 
     public GameObject player;
     public GameObject cam;
+    public Text health;
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
         if (player.transform.position.y < cam.transform.position.y - cam.transform.localScale.y)
+        {
             player.transform.position = new Vector3(0, 0, 0);
+            health.text = "Health: " + player.GetComponent<Health>().removeHP(1);
+            if (player.GetComponent<Health>().getHP() == 0)
+            {
+                player.GetComponent<PlayerBehavior>().Death();
+            }
+        }
 	}
 }
